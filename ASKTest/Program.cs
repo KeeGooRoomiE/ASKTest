0daa77cf3b782bd307f9e3aa32bd82e4f2ec8258
KeeGooRoomiE <59682188+KeeGooRoomiE@users.noreply.github.com>
2023-12-18 16:24:54 +0700

    Init

---
+++ ASKTest/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.RegularExpressions;
+using System.Xml.Linq;
+
+namespace TestLib
+{
+    public class TestProj
+    {
+        public enum ErrType
+        {
+            Error = 0,
+            System = 1,
+            DataProcess = 2,
+            Session = 3
+        }
+        
+
+        public static List<LogModel> ExportMockLogList()
+        {
+            List<LogModel> mockLogs = new List<LogModel>();
+            {
+                mockLogs.Add(new LogModel() { Time = DateTime.UtcNow, AppType = "Service", Type = ErrType.Error, Count = "3" });
+
+                return mockLogs;
+            }
+        }
+
+    }
+
+    //MODELS
+    public class LogModel
+    {
+        public DateTime Time { get; set; }
+        public string AppType { get; set; }
+        public TestProj.ErrType Type { get; set; }
+        public string? Count { get; set; }
+    }
+}
